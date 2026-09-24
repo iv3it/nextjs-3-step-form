ac7e774a6ac3eb6887bc5feb7d0e3977c1a82eb1
Kinga <kinga.sabat@gmail.com>
2026-09-25 01:55:17 +0200

    wrap ProductList in Suspense

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import Image from "next/image";
 import ProductList from "@/components/ProductList";
+import { Suspense } from "react";
 
 export default function Home() {
   return (
     <div className="flex flex-1 justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="w-full max-w-[1240px]">
-        <ProductList />
+        <Suspense fallback={null}>
+          <ProductList />
+        </Suspense>
       </main>
     </div>
   );
